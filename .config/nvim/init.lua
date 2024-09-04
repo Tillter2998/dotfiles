6f44b63892333ed6129e3bbdff8942094042b637
Tillter2998 <elrich.dev2998@gmail.com>
2024-09-04 19:52:58 -0300

    add aerospace config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,15 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 150 }
+  end,
+})
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
