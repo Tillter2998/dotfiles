6cce28ce19e8617503b8b0415a931c0741c06738
Tillter2998 <elrich.dev2998@gmail.com>
2024-12-08 23:31:14 -0400

    Update

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -1,12 +1,10 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-print("TESTING")
-
 autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
     local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
+    params.context = { only = { "source.organizeImports" } }
     -- buf_request_sync defaults to a 1000ms timeout. Depending on your
     -- machine and codebase, you may want longer. Add an additional
     -- argument after params if you find that you have to write the file
@@ -15,30 +13,30 @@ autocmd("BufWritePre", {
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
     for cid, res in pairs(result or {}) do
       for _, r in pairs(res.result or {}) do
-	if r.edit then
-	  local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-	  vim.lsp.util.apply_workspace_edit(r.edit, enc)
-	end
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
       end
     end
-    vim.lsp.buf.format({async = false})
+    vim.lsp.buf.format({ async = false })
   end
 })
 
 autocmd('LspAttach', {
   callback = function(args)
-	local client = vim.lsp.get_client_by_id(args.data.client_id)
-	if not client then return end
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
 
-	---@diagnostic disable-next-line: missing-parameter
-	if client.supports_method('textDocument/formatting') then
-	  -- Format the current buffer on saved
-	  autocmd('BufWritePre', {
-		buffer = args.buf,
-		callback = function()
-		  vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-		end,
-	  })
-	end
+    ---@diagnostic disable-next-line: missing-parameter
+    if client.supports_method('textDocument/formatting') then
+      -- Format the current buffer on saved
+      autocmd('BufWritePre', {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
   end,
 })
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -222,5 +222,19 @@ return {
       }
       require("telescope").load_extension("fzf")
     end
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ":lua require(\"go.install\").update_all_sync()"
   }
 }
